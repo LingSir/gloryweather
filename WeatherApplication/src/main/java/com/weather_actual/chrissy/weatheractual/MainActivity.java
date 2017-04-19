28dbb3245346a78b1ff6cd458b0044802d0bd417
chrissy <349747139@qq.com>
2017-04-19 19:49:06 +0800

    remote commit

---
+++ WeatherApplication/src/main/java/com/weather_actual/chrissy/weatheractual/MainActivity.java
@@ -0,0 +1,40 @@
+package com.weather_actual.chrissy.weatheractual;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton2;
+    private TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//https://github.com/LingSir/gloryweather.git
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
